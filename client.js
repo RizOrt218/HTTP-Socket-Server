c4a49c3914621ba715783a2e8b4cc35abc26a2d1
RizOrt218 <rizhiafalen@gmail.com>
2016-01-05 12:04:56 -1000

    connected with server

--- client.js
+++ client.js
@@ -1,9 +1,13 @@
 var net = require( 'net' );
 var client = net.connect ({
-  port: 8080
+  port: 8080,
+  host: 'localhost'
 },
   function ( ) { //'connect' listener
   console.log( 'connected to server!' );
+  client.write( 'GET / HTTP/1.1\n ' );
+  client.write( 'Host: localhost:8080\n' );
+  client.write( 'Cache-Control: no-cache' );
   client.write( 'word!\r\n' );
 });
 client.on( 'data', function ( data ) {
