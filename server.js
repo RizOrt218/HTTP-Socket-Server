106f40ae5445ab3db738f20207340b01d2a1ec50
RizOrt218 <rizhiafalen@gmail.com>
2016-01-04 21:59:57 -1000

    server is ruuunnnning

--- server.js
+++ server.js
@@ -8,43 +8,50 @@ function serversEventListener( incomingRequest ) {
   incomingRequest.setEncoding( 'utf-8' ); //universal coded character. to encode all possible characters
   //retrieve buffer info and parsing it
   incomingRequest.on( 'data', function( buffer ) {
-    var bufferString = buffer.split( ' ' );
-    console.log('GETTTTTT THISTSITSITSI ', bufferString);
-    var path = bufferString[ 1 ].toString( );
-    console.log( 'thists paaassss', path );
+    var bufferString = buffer.split( ' ' ); // [ GET :  ... ]
+    var uri = bufferString[ 1 ].toString( );  // index.html  || hydrogen.html  ...
+    var splitUri = uri.split( '.' ); // ex: [ 'index' , 'html' ]
+    var type = splitUri[ splitUri.length - 1]; // ex: [ html ]
+    console.log( 'thsitsoieoiw', type );
     var getDate = new Date( ).toString( );
-    var type = 'text/html';
 
-    if ( path === '/' ) {
-      path = '/index.html';
+    if ( uri === '/' ) {
+      uri = '/index.html';
+      type = 'html';
     }
-    if ( path === '/styles.css' ){
-      type = 'text/css';
+    if ( type === 'html' ) {
+      console.log( type );
+      type = 'text/html';
     }
-  // fs.readFile( './css/styles.css', function ( data ) {
-  //     incomingRequest.write( data );
-  //     incomingRequest.end( );
-  //   });
+    else {
+    type = 'text/css';
+    }
 
-    return fs.readFile( './server_file' + path, function ( err , data ) {
+    return fs.readFile( './server_file' + uri, function ( err , data ) {
       if ( err ) {
+        type = 'text/html';
         return fs.readFile( './server_file/404.html' , function( err, data ) {
-          incomingRequest.write( data );
-          return incomingRequest.end(  );   //terminate connection
+
+            incomingRequest.write( 'HTTP/ 1.1 404 Not Found\n');
+            incomingRequest.write( 'Server: Rizzy\'s Server \n' );
+            incomingRequest.write( 'Date:' + getDate + ' \n' );
+            incomingRequest.write( 'Content-Type:' + type + ';' + ' charset=utf-8\n' );
+            incomingRequest.write( 'Content-Length:' + data.length + ' \n' );
+            incomingRequest.write( 'Connection: keep-alive\n' );
+            incomingRequest.write('\n\n');
+            incomingRequest.write( data );
+            return incomingRequest.end(  );   //terminate connection
+          // }
         });
       }
-      var currentData = data.toString();
-      var stat = 'HTTP/ 1.1 200 OK\n';
-      incomingRequest.write( stat );
+      incomingRequest.write( 'HTTP/ 1.1 200 OK\n');
       incomingRequest.write( 'Server: Rizzy\'s Server \n' );
       incomingRequest.write( 'Date:' + getDate + ' \n' );
-      console.log( 'tyyyyype', type );
       incomingRequest.write( 'Content-Type:' + type + ';' + ' charset=utf-8\n' );
-      incomingRequest.write( 'Content-Length:' + currentData.length + ' \n' );
+      incomingRequest.write( 'Content-Length:' + data.length + ' \n' );
       incomingRequest.write( 'Connection: keep-alive\n' );
-      incomingRequest.write( 'Status: ' + stat + '\n' );
       incomingRequest.write('\n\n');
-      incomingRequest.write( currentData );
+      incomingRequest.write( data );
       incomingRequest.end(  );   //terminate connection
     }); // end of fs.readFile
   }); // end of incomingRequest.on
