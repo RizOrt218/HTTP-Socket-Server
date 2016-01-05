c254e66f9e6d73d7891cec96c76ac80caa52aae0
RizOrt218 <rizhiafalen@gmail.com>
2016-01-04 23:09:58 -1000

    mv css file and changed incomingRequest to socReq

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ var fs = require( 'fs' );
 
 var server = net.createServer( serversEventListener );
 
-function serversEventListener( incomingRequest ) {
+function serversEventListener( socReq ) {
   console.log( 'we got connection!' );
-  incomingRequest.setEncoding( 'utf-8' ); //universal coded character. to encode all possible characters
+  socReq.setEncoding( 'utf-8' ); //universal coded character. to encode all possible characters
   //retrieve buffer info and parsing it
-  incomingRequest.on( 'data', function( buffer ) {
+  socReq.on( 'data', function( buffer ) {
     var bufferString = buffer.split( ' ' ); // [ GET :  ... ]
     var uri = bufferString[ 1 ].toString( );  // index.html  || hydrogen.html  ...
     var splitUri = uri.split( '.' ); // ex: [ 'index' , 'html' ]
     var type = splitUri[ splitUri.length - 1]; // ex: [ html ]
-    console.log( 'thsitsoieoiw', type );
     var getDate = new Date( ).toString( );
 
     if ( uri === '/' ) {
@@ -32,29 +31,28 @@ function serversEventListener( incomingRequest ) {
         type = 'text/html';
         return fs.readFile( './server_file/404.html' , function( err, data ) {
 
-            incomingRequest.write( 'HTTP/ 1.1 404 Not Found\n');
-            incomingRequest.write( 'Server: Rizzy\'s Server \n' );
-            incomingRequest.write( 'Date:' + getDate + ' \n' );
-            incomingRequest.write( 'Content-Type:' + type + ';' + ' charset=utf-8\n' );
-            incomingRequest.write( 'Content-Length:' + data.length + ' \n' );
-            incomingRequest.write( 'Connection: keep-alive\n' );
-            incomingRequest.write('\n\n');
-            incomingRequest.write( data );
-            return incomingRequest.end(  );   //terminate connection
-          // }
+            socReq.write( 'HTTP/ 1.1 404 Not Found\n');
+            socReq.write( 'Server: Rizzy\'s Server \n' );
+            socReq.write( 'Date:' + getDate + ' \n' );
+            socReq.write( 'Content-Type:' + type + ';' + ' charset=utf-8\n' );
+            socReq.write( 'Content-Length:' + data.length + ' \n' );
+            socReq.write( 'Connection: keep-alive\n' );
+            socReq.write('\n\n');
+            socReq.write( data );
+            return socReq.end(  );   //terminate connection
         });
       }
-      incomingRequest.write( 'HTTP/ 1.1 200 OK\n');
-      incomingRequest.write( 'Server: Rizzy\'s Server \n' );
-      incomingRequest.write( 'Date:' + getDate + ' \n' );
-      incomingRequest.write( 'Content-Type:' + type + ';' + ' charset=utf-8\n' );
-      incomingRequest.write( 'Content-Length:' + data.length + ' \n' );
-      incomingRequest.write( 'Connection: keep-alive\n' );
-      incomingRequest.write('\n\n');
-      incomingRequest.write( data );
-      incomingRequest.end(  );   //terminate connection
+        socReq.write( 'HTTP/ 1.1 200 OK\n');
+        socReq.write( 'Server: Rizzy\'s Server \n' );
+        socReq.write( 'Date:' + getDate + ' \n' );
+        socReq.write( 'Content-Type:' + type + ';' + ' charset=utf-8\n' );
+        socReq.write( 'Content-Length:' + data.length + ' \n' );
+        socReq.write( 'Connection: keep-alive\n' );
+        socReq.write('\n\n');
+        socReq.write( data );
+        socReq.end(  );   //terminate connection
     }); // end of fs.readFile
-  }); // end of incomingRequest.on
+  }); // end of socReq.on
 } // end of serversEventListener( )
 
 // grab a random port.
