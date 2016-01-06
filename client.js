9e58d7d15bc2eb307c907a79222960743f8515ba
RizOrt218 <rizhiafalen@gmail.com>
2016-01-05 15:09:38 -1000

    connected with server.js

--- client.js
+++ client.js
@@ -1,19 +1,18 @@
 var net = require( 'net' );
-var client = net.connect ({
-  port: 8080,
-  host: 'localhost'
-},
-  function ( ) { //'connect' listener
+var PORT = 8080;
+var HOST = 'localhost';
+
+var client = net.connect( PORT, HOST, function ( ) { //'connect' listener
   console.log( 'connected to server!' );
-  client.write( 'GET / HTTP/1.1\n ' );
-  client.write( 'Host: localhost:8080\n' );
-  client.write( 'Cache-Control: no-cache' );
-  client.write( 'word!\r\n' );
+  client.write( 'GET / HTTP/1.1\n ' + 'Host: localhost\n\n');
+  // client.write( 'Cache-Control: no-cache' );
+
 });
-client.on( 'data', function ( data ) {
-  console.log( data.toString() );
-  client.end ( );
-});
+  client.on( 'data', function ( data ) {
+    console.log( data.toString() );
+    client.end ( );
+  });
 client.on( 'end', function ( ) {
   console.log ( 'disconnected from server' );
+  // client.end( );
 });
